e6e1b7f64d638a6d2ec08244dc30572668d58952
Thomas <thomas@craftacademy.se>
2022-01-21 14:41:49 +0100

    [HOT FIX] updates axios settings

--- src/modules/network.js
+++ src/modules/network.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "https://yesterdays-news-api.herokuapp.com/api" });
+let apiURL;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  apiURL = "http://localhost:3000/api";
+} else {
+  apiURL = "https://yesterdays-news-api.herokuapp.com/api";
+}
+
+export const api = axios.create({ baseURL: apiURL });
