cd8b41834961f5f249f56db163dd8a3a48e0ffe5
giacoletti <giacoletti92@gmail.com>
2022-01-31 17:07:10 +0100

    Adjusts title spacing, left aligned

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Typography } from "@mui/material";
-import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 const Title = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   return (
     <Typography
       data-cy="title"
       variant="h2"
-      component={Link}
-      to="/"
+      component="div"
+      onClick={() => navigate('/')}
       sx={{
-        paddingLeft: 10,
+        paddingLeft: "30px",
         textDecoration: "none",
         color: "black",
         fontFamily: "'PT Serif', serif",
